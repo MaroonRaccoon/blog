089dea58c9606ee7e78a66cd8db1d7b7485732e2
Maroon Raccoon <maroon-raccoon1@proton.me>
2025-03-28 07:29:34 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "blog";
+
+  inputs = {
+      nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import inputs.nixpkgs {
+          inherit system;
+      };
+      node = pkgs.nodePackages_latest;
+      buildInputs = with pkgs; [
+        node.mathjax
+      ];
+    in
+    {
+      defaultPackage.${system} = pkgs.stdenv.mkDerivation {
+        name = "blog";
+        inherit buildInputs;
+        src = ./.;
+      };
+
+      devShell.${system} = pkgs.mkShell {
+        inherit buildInputs;
+      };
+    };
+}
